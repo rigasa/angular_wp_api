115e1eee08760bd6c902a185549542a43af43bda
rigasa <ricardo@rigasa.pro>
2018-08-01 13:35:16 +0200

    Integrate WP Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { WordpressService } from './wordpress.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  posts$: Observable<any[]>;
   title = 'app';
+
+  constructor(private wp: WordpressService) {
+    this.posts$ = this.wp.getPosts();
+  }
 }
