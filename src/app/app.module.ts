a2de733eab1ee429fc4bde01fd8edd80fbb4495b
rigasa <ricardo@rigasa.pro>
2018-08-01 13:09:11 +0200

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app-material.module';
+import { FlexModule } from '@angular/flex-layout';
+import { HttpClientModule } from '../../node_modules/@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    FlexModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
